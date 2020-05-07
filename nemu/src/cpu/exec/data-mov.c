ec841e26bad564cff506588a2c355fd9238c1385
tracer-ics2017 <tracer@njuics.org>
2020-05-07 17:09:40 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  17:09:40 up  1:27,  2 users,  load average: 0.00, 0.00, 0.00 71126142bbdc9f217ec269e2e217568ecd2506cf

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,14 +6,15 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  TODO();
-
+  //TODO();
+  rtl_push(&id_dest->val);
   print_asm_template1(push);
 }
 
 make_EHelper(pop) {
-  TODO();
-
+  //TODO();
+  rtl_pop(&id_src->val);
+  operand_write(id_dest,&id_src->val);
   print_asm_template1(pop);
 }
 
