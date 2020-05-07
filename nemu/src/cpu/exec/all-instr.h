e095ebb41e82ddc3c994722c310699f21ad7290c
tracer-ics2017 <tracer@njuics.org>
2020-05-07 15:49:19 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  15:49:19 up 6 min,  2 users,  load average: 0.00, 0.00, 0.00 6f5250d8af3ce07278e8c666268321469b545eec

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,8 +1,11 @@
 #include "cpu/exec.h"
-
+make_EHelper(call);
+make_EHelper(pop);
+make_EHelper(push);
+make_EHelper(sub);
+make_EHelper(xor);
+make_EHelper(ret);
 make_EHelper(mov);
-
 make_EHelper(operand_size);
-
 make_EHelper(inv);
 make_EHelper(nemu_trap);
