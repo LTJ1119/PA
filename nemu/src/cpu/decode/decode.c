32370df4c068df2bd535e6eca03111036f0ac274
tracer-ics2017 <tracer@njuics.org>
2020-05-24 17:03:32 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  17:03:32 up  1:06,  2 users,  load average: 0.00, 0.00, 0.00 d8a3d735264749f8507baf919674c0f4f5d53dae

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -39,20 +39,9 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   //TODO();
-  if(op->width==4)
-  {  	op->simm=instr_fetch(eip,op->width);}
-  else if(op->width==2)
-  {
-  	t0=((uint16_t)instr_fetch(eip,op->width));
-	rtl_sext(&t1,&t0,2);
-	op->simm=t1;
-  }
-  else
-  {
-  	t0=((uint16_t)instr_fetch(eip,op->width));
-	rtl_sext(&t1,&t0,1);
-	op->simm=t1;
-  }
+  t0=instr_fetch(eip,op->width);
+  rtl_sext(&t1,&t0,op->width);
+  op->simm=t1;
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
