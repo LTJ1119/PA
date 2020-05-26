87c0a38558490cae4b272468da0b902b71610735
tracer-ics2017 <tracer@njuics.org>
2020-05-26 21:50:12 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:50:12 up  4:49,  2 users,  load average: 0.00, 0.00, 0.00 f64a267fb72af5a95d0411ae579fcc4a5ec558e

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -185,9 +185,10 @@ static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   
-  rtl_shri(dest,src1,width*8-1);
+  //rtl_shri(dest,src1,width*8-1);
   // dest <- src1[width * 8 - 1]
   //TODO();
+  *dest=((*src1)<<(32-width*8))>>31;
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
