ce7359387a7a49f5d135b9318f8b9dffd7d27ca5
tracer-ics2017 <tracer@njuics.org>
2020-05-27 16:37:33 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  16:37:33 up  7:03,  2 users,  load average: 0.00, 0.00, 0.00 4a5ebb2ce40ca9d9fea270afc45c7c1361ca6730

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -11,7 +11,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_L, CC_NL, CC_LE, CC_NLE
   };
 
-  // TODO: Query EFLAGS to determine whether the condition code is satisfied.
+  // TODO: uery EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O: rtl_get_OF(dest);break;
