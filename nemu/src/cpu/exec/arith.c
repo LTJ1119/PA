bcaf40361dcfbf680006c4e73f9877236c2a0386
tracer-ics2017 <tracer@njuics.org>
2020-05-27 16:06:25 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  16:06:25 up  6:32,  2 users,  load average: 0.01, 0.01, 0.00 30322295b8d84c8644eb30396609fbf72828b5d8

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -56,8 +56,8 @@ make_EHelper(cmp) {
   rtl_msb(&t0,&id_dest->val,id_dest->width);
   rtl_msb(&t1,&id_src->val,id_src->width);
   rtl_xor(&t3,&t0,&t1);
-  rtl_get_CF(&t1);
-  printf("CF:%d\n",t1);
+  rtl_get_ZF(&t1);
+  printf("ZF:%d\n",t1);
   rtl_get_SF(&t1);
   printf("SF:%d\n",t1);
   rtl_xor(&t1,&t0,&t1);
