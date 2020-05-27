1972bccff632f9cbc6c405b374047694b0073995
tracer-ics2017 <tracer@njuics.org>
2020-05-27 17:02:21 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  17:02:21 up  7:27,  2 users,  load average: 0.00, 0.00, 0.00 8db3dd0f1ba6db9fe86283603c7eabdc9df8e40a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -39,6 +39,7 @@ make_EHelper(cmp) {
   //TODO();
   printf("%d %d\n",id_dest->val,id_src->val);
   rtl_sub(&t0,&id_dest->val,&id_src->val);
+  printf("%d %d\n",id_dest->val,id_src->val);
   rtl_sltu(&t1,&id_dest->val,&t0);
   rtl_update_ZFSF(&t0,id_dest->width);
   rtl_sltu(&t3,&id_dest->val,&t0);
