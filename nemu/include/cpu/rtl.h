ee7b0645a47ea878b91c951b1e136d81811de6c9
tracer-ics2017 <tracer@njuics.org>
2020-05-28 11:27:38 +0800

    >  compile 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  11:27:38 up  1:56,  2 users,  load average: 0.08, 0.02, 0.01 d4693e5a46e89a247b6de7c7bf39a7ae4d1f6dcc

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -211,7 +211,8 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
-  rtl_shri(&t0,result,width*8-1);
+  //rtl_shri(&t0,result,width*8-1);
+  t0=*result>>(width*8-1);
   rtl_set_SF(&t0);// eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   //TODO();
 }
