266e9bbaf68734306c828865cb514dfc570eb005
tracer-ics2017 <tracer@njuics.org>
2020-06-12 14:33:15 +0800

    >  run 161820319 litengjiao Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  14:33:15 up  4:08,  2 users,  load average: 0.00, 0.10, 0.19 11c7e8134b2e0984efc3e20f2cf63aec2fbc0fb1

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,7 +28,8 @@ static inline uintptr_t sys_read(_RegSet *r,uintptr_t fd, uintptr_t buf, uintptr
 }
 
 static inline uintptr_t sys_lseek(_RegSet *r,uintptr_t fd, uintptr_t offset, uintptr_t whence) {
-  return fs_lseek(fd, offset, whence);
+  SYSCALL_ARG1(r)=fs_lseek(fd, offset, whence);
+  return 1;
 }
 
 static inline uintptr_t sys_close(_RegSet *r,uintptr_t fd) {
